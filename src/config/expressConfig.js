1191b7406b967aa5193821b32a683d2f7096a5c7
adilafattobene <adilafatobeni@outlook.com>
2021-02-07 14:02:36 -0300

    Add body parser to use post http resquisitions

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const consign = require("consign");
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json());
 
 consign({ cwd: "./src" })
     .include("routes")
