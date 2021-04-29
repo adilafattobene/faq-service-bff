31a36afbdd72a67bbaef32ebc977e5b692e40b60
adilafattobene <adilafatobeni@outlook.com>
2021-04-28 21:53:22 -0300

    refactor: change createJwtToken function to return promise

--- src/api/services/jwtService.js
+++ src/api/services/jwtService.js
@@ -1,17 +1,23 @@
 const jwt = require("jsonwebtoken");
-require('dotenv').config();
+require("dotenv").config();
 
 exports.createJwtToken = (payload) => {
-    const token =  jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_TIME,
-    });
+  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_TIME,
+  });
 
-    return token;
-}
+  return token;
+};
 
-exports.verifyToken = ( token, next ) => {
-    jwt.verify(token, process.env.JWT_SECRET, function(err, decoded) {
-        if (err) throw err;
-        next(decoded);
+exports.verifyToken = (token, next) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, function (err, decoded) {
+      if (err) {
+        reject(err);
+        return;
+      }
+    
+      resolve(decoded);
     });
-}
+  });
+};
