4ef449ff711c62d1a882ae31de508996f9ea47fc
adilafattobene <adilafatobeni@outlook.com>
2021-05-08 21:22:43 -0300

    feat: add cmsServiceClient unit test

--- src/api/clients/cmsServiceClient.test.js
+++ src/api/clients/cmsServiceClient.test.js
@@ -6,12 +6,10 @@ var MockAdapter = require("axios-mock-adapter");
 // This sets the mock adapter on the default instance
 var mock = new MockAdapter(axios);
 
-describe("get faqs unit tests", () => {
+describe("getFaqs unit tests", () => {
   afterEach(() => mock.resetHandlers());
 
-  test("get user should return a user when it receive a userId", async () => {
-    // Mock any GET request
-    // arguments for reply are (status, data, headers)
+  test("should return all faqs when it receive a profile", async () => {
     const faqs = [
       {
         id: 1,
@@ -38,5 +36,55 @@ describe("get faqs unit tests", () => {
     const a = await client.getFaq("public");
 
     expect(a.faqs).toEqual(faqs);
+  });
+
+  test("should throw an error when cms request fail", async () => {
+    mock
+      .onGet("http://localhost:1337/faqs?type=public")
+      .reply(400);
+
+      await expect(client.getFaq("public")).rejects.toThrow(Error);
+  });
+});
+
+describe("getFaqById unit tests", () => {
+  afterEach(() => mock.resetHandlers());
+
+  test("should return a faq when it receive a slug", async () => {
+
+    const faq = {
+      id: 1,
+      slug: "como-se-cadastrar",
+      title: "Como se cadastrar",
+      subtitle: null,
+      content: "Isso ai",
+      active: true,
+      type: "public",
+      createdAt: null,
+      modifiedAt: null,
+      user: null,
+      published_at: "2021-04-06T22:43:13.610Z",
+      created_at: "2021-04-06T22:43:09.919Z",
+      updated_at: "2021-04-06T22:43:13.665Z",
+      created_for: null,
+    };
+
+    mock
+      .onGet("http://localhost:1337/faqs?type=public&slug=como-se-cadastrar")
+      .reply(200, {
+        faq: faq,
+      });
+
+    const response = await client.getFaqBySlug("public", "como-se-cadastrar");
+
+    expect(response.faq).toEqual(faq);
+  });
+
+  test("should throw an error when cms request fail", async () => {
+    mock
+      .onGet("http://localhost:1337/faqs?type=public&slug=como-se-cadastrar")
+      .reply(400);
+
+      await expect(client.getFaqBySlug("public", "como-se-cadastrar")).rejects.toThrow(Error);
   });
 });
