b64ad13597523860e86d7cf8c0c91a6f8ffb6f3f
adilafattobene <adilafatobeni@outlook.com>
2021-05-10 19:10:30 -0300

    test: implement faqController unit tests

--- src/api/controller/faqController.test.js
+++ src/api/controller/faqController.test.js
@@ -1,67 +1,162 @@
-const service = require("../services/faqService");
 const controller = require("./faqController");
 
-const { compareSync } = require("bcrypt");
 const service = require("../services/faqService");
 
 describe("getFaq unit test", () => {
   test("should return 200", async () => {
-    //TODO
+    const faqs = [
+      {
+        id: 1,
+        slug: "como-se-cadastrar",
+        title: "Como se cadastrar",
+        subtitle: null,
+        content: "Isso ai",
+        active: true,
+        type: "public",
+        createdAt: null,
+        modifiedAt: null,
+        user: null,
+        published_at: "2021-04-06T22:43:13.610Z",
+        created_at: "2021-04-06T22:43:09.919Z",
+        updated_at: "2021-04-06T22:43:13.665Z",
+        created_for: null,
+      },
+    ];
+
+    jest.spyOn(service, "getFaqs").mockResolvedValue(faqs);
+
+    const res = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn();
+
+    const a = await controller.getFaqs(
+      { headers: { "x-access-token": "blabla" } },
+      res
+    );
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(faqs);
   });
 
   test("should return 404", async () => {
-   //TODO
+    jest.spyOn(service, "getFaqs").mockRejectedValue(new Error("NotFound"));
+
+    const res = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+
+    const a = await controller.getFaqs(
+      { headers: { "x-access-token": "blabla" } },
+      res
+    );
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ message: "FAQ not found." });
   });
 
   test("should return 500", async () => {
-   //TODO
+    jest.spyOn(service, "getFaqs").mockRejectedValue(new Error("OtherError"));
+
+    const res = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+
+    const a = await controller.getFaqs(
+      { headers: { "x-access-token": "blabla" } },
+      res
+    );
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Something is wrong - FAQ not found.",
+    });
   });
 });
-
-
-// exports.getFaq = async (req, res) => {
-//   const token = req.headers["x-access-token"];
-
-//   try {
-//     const response = await service.getFaq(token);
-
-//     return res.status(200).json(response);
-//   } catch (err) {
-//     if (err.message === "NotFound") {
-//       return res.status(404).json({ message: "FAQ not found." });
-//     }
-
-//     return res.status(500).send("Erro Requisição getUser " + err);
-//   }
-// };
 
 describe("getFaqBySlug unit test", () => {
   test("should return 200", async () => {
-    //TODO
+    const faq = {
+      id: 1,
+      slug: "como-se-cadastrar",
+      title: "Como se cadastrar",
+      subtitle: null,
+      content: "Isso ai",
+      active: true,
+      type: "public",
+      createdAt: null,
+      modifiedAt: null,
+      user: null,
+      published_at: "2021-04-06T22:43:13.610Z",
+      created_at: "2021-04-06T22:43:09.919Z",
+      updated_at: "2021-04-06T22:43:13.665Z",
+      created_for: null,
+    };
+
+    jest.spyOn(service, "getFaqBySlug").mockResolvedValue(faq);
+
+    const res = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn();
+
+    const a = await controller.getFaqBySlug(
+      {
+        headers: { "x-access-token": "blabla" },
+        params: { slug: "como-se-cadastrar" },
+      },
+      res
+    );
+
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith(faq);
   });
 
   test("should return 404", async () => {
-   //TODO
+    jest
+      .spyOn(service, "getFaqBySlug")
+      .mockRejectedValue(new Error("NotFound"));
+
+    const res = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+
+    const a = await controller.getFaqBySlug(
+      {
+        headers: { "x-access-token": "blabla" },
+        params: { slug: "como-se-cadastrar" },
+      },
+      res
+    );
+
+    expect(res.status).toHaveBeenCalledWith(404);
+    expect(res.json).toHaveBeenCalledWith({ message: "FAQ not found." });
   });
 
   test("should return 500", async () => {
-   //TODO
+    jest
+      .spyOn(service, "getFaqBySlug")
+      .mockRejectedValue(new Error("OtherError"));
+
+    const res = {};
+    res.send = jest.fn().mockReturnValue(res);
+    res.status = jest.fn().mockReturnValue(res);
+    res.json = jest.fn().mockReturnValue(res);
+
+    const a = await controller.getFaqBySlug(
+      {
+        headers: { "x-access-token": "blabla" },
+        params: { slug: "como-se-cadastrar" },
+      },
+      res
+    );
+
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Something is wrong - FAQ not found.",
+    });
   });
 });
-
-
-// exports.getFaqBySlug = async (req, res) => {
-//   const token = req.headers["x-access-token"];
-
-//   try {
-//     const response = await service.getFaqBySlug(token, req.params.slug);
-
-//     return res.status(200).json(response);
-//   } catch (err) {
-//     if (err.message === "NotFound") {
-//       return res.status(404).json({ message: "FAQ not found." });
-//     }
-
-//     return res.status(500).send("Erro Requisição getUser " + err);
-//   }
-// };
