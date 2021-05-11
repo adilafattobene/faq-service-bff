1bd3980aa475280adc88b2be12a7e8ce028e21f7
adilafattobene <adilafatobeni@outlook.com>
2021-05-11 10:04:29 -0300

    fix: fix getFaq route

--- src/routes/faqRoute.js
+++ src/routes/faqRoute.js
@@ -1,6 +1,6 @@
 const controller = require("../api/controller/faqController");
 
 module.exports = (app) => {
-  app.get("/faq", controller.getFaq);
+  app.get("/faq", controller.getFaqs);
   app.get("/faq/:slug", controller.getFaqBySlug);
 };
