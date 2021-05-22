7fd4c3e23d226e5e9014d3fa9a69be20bfb30105
adilafattobene <adilafatobeni@outlook.com>
2021-05-21 23:03:57 -0300

    feat: add cors config into expressConfig file

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const consign = require("consign");
-const app = express();
-const bodyParser = require('body-parser')
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
+const app = express();
+
+app.use(cors());
 app.use(bodyParser.json());
 
-consign({ cwd: "./src" })
-    .include("routes")
-    .into(app);
+consign({ cwd: "./src" }).include("routes").into(app);
 
 module.exports = app;
