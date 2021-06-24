d9b36e54c0ffa69eb116807d437ba1916a5051d0
adilafattobene <adilafatobeni@outlook.com>
2021-06-24 10:25:43 -0300

    dswl-feat: create getUser and get profile model sql

---
+++ src/api/models/userModel.js
@@ -0,0 +1,39 @@
+const dbConnection = require("../../config/dbServer");
+
+exports.getUserProfile = async function (userId) {
+  let connection = dbConnection();
+
+  connection.connect(function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Conectado");
+  });
+
+  let sql = "select * from login where account_id=$1";
+
+  const a = await connection.query(sql, [userId]);
+
+  connection.end();
+
+  return a.rows[0];
+};
+
+exports.getUser = async function (userId) {
+  let connection = dbConnection();
+
+  connection.connect(function (err) {
+    if (err) {
+      throw err;
+    }
+    console.log("Conectado");
+  });
+
+  let sql = "select * from account where id=$1";
+
+  const a = await connection.query(sql, [userId]);
+
+  connection.end();
+
+  return a.rows[0];
+};
